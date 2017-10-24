729e3ac7c5c6b68516f6d4591c13445d001e124f
Vincent Composieux <vincent.composieux@gmail.com>
2017-10-24 13:42:45 +0200

    Updated example to b more clear

--- example/main.go
+++ example/main.go
@@ -7,19 +7,23 @@ import (
 	"github.com/unikorp/workmq"
 )
 
+func handleProcessor(worker *workmq.Worker, message workmq.Message) {
+	fmt.Printf("Worker #%d (queue: \"%s\") manages message %s\n", worker.ID, worker.Queue, message.Bod)
+}
+
 func main() {
 	app := workmq.Init()
 
 	app.AddProcessor("processor.logger.1s", func(worker *workmq.Worker, message workmq.Message) {
 		time.Sleep(time.Second * 1)
 
-		fmt.Printf("Worker #%d (queue: \"%s\") manages message %s\n", worker.ID, worker.Queue, message.Body)
+		handleProcessor(worker, message)
 	})
 
 	app.AddProcessor("processor.logger.2s", func(worker *workmq.Worker, message workmq.Message) {
 		time.Sleep(time.Second * 2)
 
-		fmt.Printf("Worker #%d (queue: \"%s\") manages message %s\n", worker.ID, worker.Queue, message.Body)
+		handleProcessor(worker, message)
 	})
 
 	app.Handle()
